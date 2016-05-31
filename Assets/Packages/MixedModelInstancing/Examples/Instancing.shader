4e1bca5c05d00b1868896f5161856cdb2901c00b
Nakata Nobuyuki <nobu.nakata@gmail.com>
2016-06-01 02:15:19 +0900

    no message

---
+++ Assets/Packages/MixedModelInstancing/Examples/Instancing.shader
@@ -0,0 +1,49 @@
+﻿Shader "Unlit/Instancing" {
+	Properties {
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass {
+			CGPROGRAM
+			#pragma target 5.0
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct v2f {
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+
+			StructuredBuffer<int> _TriangleBuf;
+			StructuredBuffer<float3> _VertexBuf;
+			StructuredBuffer<float2> _UvBuf;
+			StructuredBuffer<float4x4> _WorldMatBuf;
+			
+			v2f vert (uint triId : SV_VertexID, uint instId : SV_InstanceID) {
+				int vid = _TriangleBuf[triId];
+				float3 vertex = _VertexBuf[vid];
+				float2 uv = _UvBuf[vid];
+				float4x4 world = _WorldMatBuf[instId];
+
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_VP, mul(world, float4(vertex, 1.0)));
+				o.uv = TRANSFORM_TEX(uv, _MainTex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target {
+				fixed4 col = tex2D(_MainTex, i.uv);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/Packages/MixedModelInstancing/Instancing.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace MixedModelInstancing {
+
+	public class Instancing : MonoBehaviour {
+		public static readonly float InvLogTwo = 1f / Mathf.Log (2);
+
+		public Mesh mesh;
+
+		public Material mat;
+		public int pass = 0;
+		public string propTriangleBuf = "_TriangleBuf";
+		public string propVertexBuf = "_VertexBuf";
+		public string propUvBuf = "_UvBuf";
+		public string propTransformBuf = "_WorldMatBuf";
+
+		public Transform[] instanceTransforms;
+
+		MeshBuf _meshBuf;
+		Matrix4x4[] _matrices;
+		ComputeBuffer _matrixBuf;
+
+		void OnEnable() {
+			_meshBuf = new MeshBuf (mesh);
+		}
+		void OnDisable() {
+			if (_meshBuf != null)
+				_meshBuf.Dispose ();
+			if (_matrixBuf != null)
+				_matrixBuf.Dispose ();
+		}
+		void OnRenderObject() {
+			var len = CeilToNearestPowerOfTwo (instanceTransforms.Length);
+			if (len <= 0)
+				return;
+
+			if (_matrices == null || _matrices.Length != len)
+				_matrices = new Matrix4x4[len];
+			for (var i = 0; i < instanceTransforms.Length; i++)
+				_matrices [i] = instanceTransforms [i].localToWorldMatrix;
+			if (instanceTransforms.Length < len)
+				System.Array.Clear (_matrices, instanceTransforms.Length, len - instanceTransforms.Length);
+
+			if (_matrixBuf == null || _matrixBuf.count != len) {
+				if (_matrixBuf != null)
+					_matrixBuf.Dispose ();
+				_matrixBuf = Create(_matrices);
+			}
+			_matrixBuf.SetData (_matrices);
+
+			mat.SetPass (0);
+			mat.SetBuffer (propTriangleBuf, _meshBuf.triangles);
+			mat.SetBuffer (propVertexBuf, _meshBuf.vertices);
+			mat.SetBuffer (propUvBuf, _meshBuf.uv);
+			mat.SetBuffer (propTransformBuf, _matrixBuf);
+			Graphics.DrawProcedural (MeshTopology.Triangles, _meshBuf.vertexCount, instanceTransforms.Length);
+		}
+
+		public static ComputeBuffer Create<T>(T[] array) {
+			return new ComputeBuffer (array.Length, Marshal.SizeOf (typeof(T)));
+		}
+		public static ComputeBuffer Convert<T>(T[] array) {
+			var buf = Create (array);
+			buf.SetData (array);
+			return buf;
+		}
+		public static int CeilToNearestPowerOfTwo (int len) {
+			return len > 0 ? (1 << Mathf.CeilToInt (Mathf.Log (len) * InvLogTwo)) : 0;
+		}
+
+		public class MeshBuf : System.IDisposable {
+			public readonly Mesh mesh;
+			public readonly int vertexCount;
+			public readonly ComputeBuffer triangles;
+			public readonly ComputeBuffer vertices;
+			public readonly ComputeBuffer uv;
+
+			public MeshBuf(Mesh mesh) {
+				this.mesh = mesh;
+				this.triangles = Convert(mesh.triangles);
+				this.vertices = Convert(mesh.vertices);
+				this.uv = Convert(mesh.uv);
+				this.vertexCount = this.triangles.count;
+			}
+
+			#region IDisposable implementation
+			public void Dispose () {
+				triangles.Dispose ();
+				vertices.Dispose ();
+				uv.Dispose ();
+			}
+			#endregion
+		}
+	}
+}
